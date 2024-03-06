504ab0e00c24dc3a666673555e07069f312d8df5
MrGeek-zrh <mrgeek-zrh@outlook.com>
2024-03-06 23:20:32 +0800

    feat: add more func to debug

--- include/soc.hpp
+++ include/soc.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <sstream>  // for std::stringstream
+#include <iostream>
+#include <iomanip>  // for std::setw and std::setfill
 #include "emulator.hpp"
 #include "memory.hpp"
 #include <verilated.h>
@@ -9,6 +12,7 @@
 #include <fstream>
 #include <fmt/core.h>
 #include "VHeliosX___024root.h"
+#include "verilatedos.h"
 
 #define COMMIT_TIMEOUT 5000
 
@@ -40,6 +44,64 @@ namespace heliosxsimulator {
             last_pc = 0x0;
         }
 
+        std::string to_hex_string_with_prefix(uint64_t value, int width) {
+            std::stringstream stream;
+            stream << "0x" << std::setfill(' ') << std::setw(width) << std::hex
+                   << value;
+            return stream.str();
+        }
+
+        vluint64_t rob_valid =
+            cpu_top->rootp->HeliosX__DOT__u_SingleInstROB__DOT__valid;
+
+        vluint64_t rob_finish =
+            cpu_top->rootp->HeliosX__DOT__u_SingleInstROB__DOT__finish;
+
+        vluint64_t rob_dstvalid =
+            cpu_top->rootp->HeliosX__DOT__u_SingleInstROB__DOT__dstValid;
+
+        struct VlUnpacked<uint32_t, 64> inst_pc =
+            cpu_top->rootp->HeliosX__DOT__u_SingleInstROB__DOT__inst_pc;
+
+        struct VlUnpacked<uint8_t, 64> dstnum =
+            cpu_top->rootp->HeliosX__DOT__u_SingleInstROB__DOT__dst;
+
+        void print_rob() {
+            // 设置列宽
+            const int width_hex = 18;  // 16进制的列宽，增加以适应0x前缀
+            const int width_dec = 5;  // 10进制的列宽
+            const int width_tag = 8;  // rrftag列宽
+
+            // 打印表头
+            std::cout << std::left << std::setfill(' ') << std::setw(width_tag)
+                      << "rrftag" << std::setw(width_hex) << "finish"
+                      << std::setw(width_hex) << "valid" << std::setw(width_hex)
+                      << "dstValid" << std::setw(width_hex) << "inst_pc"
+                      << std::setw(width_dec) << "dst" << std::endl;
+
+            // 打印各行数据
+            for (int i = 0; i < 64; ++i) {
+                std::cout << std::left << std::setfill(' ') << std::dec
+                          << std::setw(width_tag) << i << std::setw(width_hex)
+                          << to_hex_string_with_prefix((rob_finish >> i) & 1, 1)
+                          << std::setw(width_hex)
+                          << to_hex_string_with_prefix((rob_valid >> i) & 1, 1)
+                          << std::setw(width_hex)
+                          << to_hex_string_with_prefix((rob_dstvalid >> i) & 1,
+                                                       1)
+                          << std::setw(width_hex)
+                          << to_hex_string_with_prefix(inst_pc[i], 8)
+                          << std::dec << std::setw(width_dec)
+                          << static_cast<unsigned>(dstnum[i]) << std::endl;
+            }
+            // 打印表头
+            std::cout << std::left << std::setfill(' ') << std::setw(width_tag)
+                      << "rrftag" << std::setw(width_hex) << "finish"
+                      << std::setw(width_hex) << "valid" << std::setw(width_hex)
+                      << "dstValid" << std::setw(width_hex) << "inst_pc"
+                      << std::setw(width_dec) << "dst" << std::endl;
+        }
+
         uint64_t get_rrf_rrfvalid(uint64_t rrftag) {
             vluint64_t rrfvalid =
                 cpu_top->rootp
@@ -47,6 +109,7 @@ namespace heliosxsimulator {
             vluint64_t mask = 0x1l << rrftag;
             return (rrfvalid & mask) >> rrftag;
         }
+
         const char *regs[32] = {
             "$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
             "a1", "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
@@ -154,6 +217,7 @@ namespace heliosxsimulator {
                         "{:#x}",
                         debug_pc_o, debug_wen, debug_wreg_num,
                         reg_idx2str(debug_wreg_num), debug_wreg_data);
+
                     running = false;
                 } else {
 #ifdef DEBUG
@@ -185,6 +249,14 @@ namespace heliosxsimulator {
                     last_pc = debug_pc_o;
                 }
             }
+
+#ifdef DEBUG
+            /* if (sim_time == 1490) { */
+            /*     fmt::println("================当前sim_time:{}", sim_time); */
+            /*     print_rob(); */
+            /*     fmt::println("================"); */
+            /* } */
+#endif  // DEBUG
         }
 
         virtual void setup() {
